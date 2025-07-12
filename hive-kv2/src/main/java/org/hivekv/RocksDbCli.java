3a12292aba8b24709dd7e75e8f4ca587a6f4b9cb
Rui Wu <wurui90@gmail.com>
2025-07-11 23:50:36 -0700

    use rocksdb

--- hive-kv2/src/main/java/org/hivekv/RocksDbCli.java
+++ hive-kv2/src/main/java/org/hivekv/RocksDbCli.java
@@ -1,5 +1,8 @@
 package org.hivekv;
 
+import org.rocksdb.RocksDB;
+import org.rocksdb.RocksDBException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RocksDbCli {
@@ -12,6 +15,29 @@ public class RocksDbCli {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = test test test" + i + 1);
+        }
+
+        try {
+            RocksDB db;
+            db = RocksDB.open("rocksdb_dir");
+            String key = "12";
+            String value = "34";
+            db.put(key.getBytes(), value.getBytes());
+            db.close();
+        } catch (RocksDBException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            RocksDB db;
+            db = RocksDB.open("rocksdb_dir");
+            String key = "12";
+            System.out.println(new String(db.get(key.getBytes())));
+            db.close();
+        } catch (RocksDBException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
         }
     }
 }
